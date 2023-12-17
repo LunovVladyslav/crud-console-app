1e883265bcdd7433df8348175593d26d5f46ed3d
LunovVladyslav <lunovvladyslav@gmail.com>
2023-12-17 23:40:39 +0100

    Writer service completed. Writer controller in progress. TODO: complete WriterController methods

--- src/main/java/com/project/consolecrud/controller/WriterController.java
+++ src/main/java/com/project/consolecrud/controller/WriterController.java
@@ -1,2 +1,51 @@
-package com.project.consolecrud.controller;public class WriterController {
+package com.project.consolecrud.controller;
+
+import com.project.consolecrud.model.Writer;
+import com.project.consolecrud.service.WriterService;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Scanner;
+
+@Controller
+public class WriterController {
+    private WriterService service;
+
+    public WriterController(WriterService service) {
+        this.service = service;
+    }
+
+    public void addWriter() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the writer's first and last name:");
+            Writer writer = new Writer();
+            writer.setFirstName(sc.next());
+            writer.setLastName(sc.next());
+            service.addWriter(writer);
+        } catch (Exception e) {
+            System.out.println("Writer can't be added");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void showAllWriters() {
+        try {
+            List<Writer> writerList = service.findAll();
+            if (writerList.isEmpty()) {
+                throw new NullPointerException();
+            }
+            for (Writer writer : writerList) {
+                System.out.printf("ID:%d. Name: %s %s.%n",
+                        writer.getId(),
+                        writer.getFirstName(), writer.getLastName());
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Any writer not found!");
+        }
+    }
+
+
+
+
+
 }
--- src/main/java/com/project/consolecrud/service/WriterService.java
+++ src/main/java/com/project/consolecrud/service/WriterService.java
@@ -1,2 +1,76 @@
-package com.project.consolecrud.service;public class WriterService {
+package com.project.consolecrud.service;
+
+import com.project.consolecrud.model.Writer;
+import com.project.consolecrud.repository.WriterRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class WriterService {
+    private WriterRepository repository;
+
+    public WriterService(WriterRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addWriter(Writer writer) {
+        try {
+            repository.save(writer);
+            System.out.println("Writer successfully added.");
+        } catch (Exception e) {
+            System.out.println("Something wrong! Writer not added");
+        }
+    }
+
+    public List<Writer> findAll() {
+        List<Writer> writerList = repository.findAll();
+
+        if (writerList.isEmpty()) {
+            System.out.println("Writers not found");
+            return Collections.emptyList();
+        }
+        return writerList;
+    }
+
+    public Writer findByName(String firstName, String lastName) throws NullPointerException {
+        Writer writer = repository.findByName(firstName, lastName);
+        if (Objects.isNull(writer)) {
+            System.out.println("Writer not found");
+            return null;
+        }
+        return writer;
+    }
+
+    public Writer findById(Long id) {
+        Writer writer = repository.findById(id);
+        if (Objects.isNull(writer)) {
+            System.out.println("Writer not found");
+            return null;
+        }
+        return writer;
+    }
+
+    public void update(Writer writer) {
+        try {
+            repository.update(writer);
+            System.out.printf("Writer %s %s successfully updated%n",
+                    writer.getFirstName(), writer.getLastName());
+        } catch (Exception e) {
+            System.out.println("Something wrong, operation aborted!");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void deleteById(Long id) {
+        try {
+            repository.deleteById(id);
+            System.out.println("Writer successfully deleted");
+        }  catch (Exception e) {
+            System.out.println("Something wrong, operation aborted!");
+            System.out.println(e.getMessage());
+        }
+    }
 }
